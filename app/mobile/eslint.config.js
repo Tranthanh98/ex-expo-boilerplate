4da79162539bca2dbc09f652be75abb7d003ca5f
Thanh Tran <trantienthanh2412@gmail.com>
2026-01-09 08:34:49 +0700

    chore: update expo and typescript configurations for better DX

--- app/mobile/eslint.config.js
+++ app/mobile/eslint.config.js
@@ -1,10 +1,25 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+    settings: {
+      "import/resolver": {
+        typescript: {},
+      },
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: "./tsconfig.json",
+      tsconfigRootDir: "./",
+    },
+    plugins: ["@typescript-eslint", "import"],
+    extends: [
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    ],
   },
 ]);
